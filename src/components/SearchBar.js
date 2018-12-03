aa1922c901d47ba8918eae1b7da067636e58e60b
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-03 17:22:41 +0300

    Create component for search users

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default ({ term, data, update }) => {
+    const dataSearch = event => {
+        const value = event.target.value.toLowerCase();
+        const filter = data.filter(user => {
+            return user.name.toLowerCase().includes(value);
+        });
+
+        update({
+            data: filter,
+            active: 0,
+            term: value
+        });
+    };
+    return (
+        <div className="searchbar form-group">
+            <input
+                value={term}
+                type="text"
+                className="form-control"
+                placeholder="Search people by name..."
+                onChange={dataSearch}
+            />
+        </div>
+    );
+};
