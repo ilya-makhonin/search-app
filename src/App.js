a579a7a35a5c35083238cad72f5a937bd78b4b11
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-03 17:24:13 +0300

    Update App Component. Add searching users and sort users

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import load from './utils/load';
 import UserList from './components/UserList';
 import ActiveUser from './components/ActiveUser';
+import SearchBar from './components/SearchBar';
+import ToolBar from './components/ToolBar';
 
 
 class App extends Component {
@@ -12,6 +14,7 @@ class App extends Component {
         active: 0
     };
     url = 'https://jsonplaceholder.typicode.com/users';
+    initialData = [];
 
     loadData = () => {
         load(this.url)
@@ -24,6 +27,7 @@ class App extends Component {
                         email: item.email
                     };
                 });
+                this.initialData = users;
                 this.setState({ data: users });
             })
             .catch(error => {
@@ -42,6 +46,24 @@ class App extends Component {
     render() {
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <SearchBar
+                            term={this.state.term}
+                            data={this.initialData}
+                            update={this.updateData}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <ToolBar
+                            initialData={this.initialData}
+                            data={this.state.data}
+                            update={this.updateData}
+                        />
+                    </div>
+                </div>
                 <div className="row">
                     <div className="col-sm-4 col-md-3 col-lg-3">
                         <ActiveUser data={this.state.data} active={this.state.active} />
