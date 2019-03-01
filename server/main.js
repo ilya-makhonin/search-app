269e6ebf52fad18f3e4d978ed99ec81645be0a19
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-01 21:35:13 +0300

    Created simple server for testing

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request-promise');
 const morgan = require('morgan');
 const path = require('path');
 
@@ -12,6 +13,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, '../build')));
 
+app.get('/users', async function(req, res) {
+    try {
+        const result = await request({
+            method: 'GET',
+            uri: 'http://jsonplaceholder.typicode.com/users',
+            json: true
+        });
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        let errorMessage = 'Error: ' + error;
+        res.json({ state: errorMessage });
+    }
+});
+
 app.listen(app.get('port'), () => {
-    console.log(`[OK] Server is running on localhost:${app.get('port')}`);
+    console.log(`[OK] Server is running on http://localhost:${app.get('port')}`);
 });
